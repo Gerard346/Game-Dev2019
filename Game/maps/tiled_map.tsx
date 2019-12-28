acdef206639e8e76a7f60bf2a15a347007718033
Gerard346 <gerard-ho@hotmail.com>
2019-12-28 20:09:40 +0100

    Delete build

--- Game/maps/tiled_map.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.5" name="tiled_map" tilewidth="16" tileheight="16" tilecount="950" columns="38">
- <image source="../../../../vacaroxa--generic-run-n-gun-pack--v.1.0/tiles_map.png" width="608" height="400"/>
-</tileset>
--- Game/maps/tiles_map.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.5" name="tiles_map" tilewidth="16" tileheight="16" tilecount="950" columns="38">
- <image source="tiles_map.png" width="608" height="400"/>
-</tileset>
